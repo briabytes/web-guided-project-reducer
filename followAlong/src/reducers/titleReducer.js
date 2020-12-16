7a20d3282b874df4f0cd5a755f56a4337cf1b5ee
Josh Jacobson <joshjacobson14@gmail.com>
2020-12-15 18:49:58 -0800

    lecture work-in-progress

--- followAlong/src/reducers/titleReducer.js
+++ followAlong/src/reducers/titleReducer.js
@@ -1,13 +1,13 @@
-import {TOGGLE_EDITING, UPDATE_TITLE} from "../actions/titleActions";
+import { TOGGLE_EDITING, UPDATE_TITLE} from './../actions/titleActions';
 
 const titleReducer = (state, action) => {
   // based on action.type, build & return a new state object
   // building a new object from scratch every time is the "immutable state" pattern
   switch(action.type) {
-    case('TOGGLE_EDITING'):
+    case(TOGGLE_EDITING):
       console.log("toggle editing case in the titleReducer");
       return {...state, editing: !state.editing};
-    case('UPDATE_TITLE'):
+    case(UPDATE_TITLE):
       console.log("update title case in the titleReducer");
       return {...state, editing: false, title: action.payload};
     default:

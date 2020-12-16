9d2f916001f5692cc06497b1a4f1526b39422cc8
Josh Jacobson <joshjacobson14@gmail.com>
2020-12-15 17:49:13 -0800

    lecture work-in-progress

--- followAlong/src/reducers/titleReducer.js
+++ followAlong/src/reducers/titleReducer.js
@@ -6,6 +6,9 @@ const titleReducer = (state, action) => {
   switch(action.type) {
     case('TOGGLE_EDITING'):
       return state;
-      
+    case('UPDATE_TITLE'):
+      return state;
+    default:
+      return state;
   }
 }

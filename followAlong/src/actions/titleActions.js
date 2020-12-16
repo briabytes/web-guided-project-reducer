de604ab90db6859e31e324f397301d8e5c3b42a9
Josh Jacobson <joshjacobson14@gmail.com>
2020-12-15 18:46:04 -0800

    lecture work-in-progress

--- followAlong/src/actions/titleActions.js
+++ followAlong/src/actions/titleActions.js
@@ -3,9 +3,11 @@ export const TOGGLE_EDITING = "TOGGLE_EDITING";
 
 export default {
     updateTitle: (newTitle)=> {
+        console.log("updateTitle action creator");
         return({type: UPDATE_TITLE, payload: newTitle})
     },
     toggleEditing: (isEditing) => {
+        console.log("toggleEditing action creator");
         return({type: TOGGLE_EDITING})
     }
 }

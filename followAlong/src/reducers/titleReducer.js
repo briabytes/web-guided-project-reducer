11b32548baeab8d8f92ffbcc91acb7d8f3452f09
Josh Jacobson <joshjacobson14@gmail.com>
2020-12-15 17:47:38 -0800

    lecture work-in-progress

--- followAlong/src/reducers/titleReducer.js
+++ followAlong/src/reducers/titleReducer.js
@@ -1,9 +1,11 @@
 
 
 const titleReducer = (state, action) => {
-    // based on action.type, build & return a new state object
-    // building a new object from scratch every time is the "immutable state" pattern
-    switch(action.type) {
-
-    }
+  // based on action.type, build & return a new state object
+  // building a new object from scratch every time is the "immutable state" pattern
+  switch(action.type) {
+    case('TOGGLE_EDITING'):
+      return state;
+      
+  }
 }

bb4b1802065649cdbe7874f152ce6221477304b6
Josh Jacobson <joshjacobson14@gmail.com>
2020-12-15 18:47:38 -0800

    lecture work-in-progress

--- followAlong/src/reducers/titleReducer.js
+++ followAlong/src/reducers/titleReducer.js
@@ -5,8 +5,10 @@ const titleReducer = (state, action) => {
   // building a new object from scratch every time is the "immutable state" pattern
   switch(action.type) {
     case('TOGGLE_EDITING'):
+      console.log("toggle editing case in the titleReducer");
       return {...state, editing: !state.editing};
     case('UPDATE_TITLE'):
+      console.log("update title case in the titleReducer");
       return {...state, editing: false, title: action.payload};
     default:
       return state;

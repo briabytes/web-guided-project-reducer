47f827e7d9ae27f1a94263b6f47bd08d3e2dc505
Josh Jacobson <joshjacobson14@gmail.com>
2020-12-15 18:32:48 -0800

    lecture work-in-progress

--- followAlong/src/actions/titleActions.js
+++ followAlong/src/actions/titleActions.js
@@ -1,2 +1,8 @@
 export const UPDATE_TITLE = "UPDATE_TITLE";
 export const TOGGLE_EDITING = "TOGGLE_EDITING";
+
+export default {
+    updateTitle: (newTitle)=> {
+        return({type: UPDATE_TITLE, payload: newTitle} )
+    }
+}
